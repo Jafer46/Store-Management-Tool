8b24684006fc06978fb898c8080d1490a3648cea
Jafer Hussein <123theelite@gmail.com>
2025-05-29 23:51:39 -0700

    setting up error handler middleware

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import "dotenv/config";
 import dbConnect from "./config/db";
+import { errorHandler } from "./middleware/error.middleware";
 
 dbConnect();
 
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use("/api", require("./modules/module.route"));
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
---
+++ backend/src/middleware/error.middleware.ts
@@ -0,0 +1,14 @@
+import { ResponseBuilder } from "@/utils/response.builder";
+
+export const errorHandler = (err: any, req: any, res: any, next: any) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  const message = err.message ? err.message : "Something went wrong";
+
+  const response = new ResponseBuilder()
+    .setData(null)
+    .setMessage(message)
+    .setSuccess(false)
+    .build();
+
+  res.status(statusCode).json(response);
+};
