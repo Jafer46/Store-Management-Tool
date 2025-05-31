59644b0087574115219f2e479b3ebfb9c72acd8d
Jafer Hussein <123theelite@gmail.com>
2025-05-31 05:14:55 -0700

    setting up shadcn and small customization

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,15 @@
 import FormBuilder from "./components/form/form-builder";
+import { Input } from "./components/ui/input";
 import { MRole } from "./constants/models";
 
 export default function App() {
   return (
-    <div className="bg-red-400">
-      <h1>hello</h1>
-      <FormBuilder obj={MRole} />
+    <div className="flex justify-center items-center h-[100vh]">
+      <div className="w-80 h-80 border-2 shadow-2xl p-4">
+        <h1>hello</h1>
+        <FormBuilder obj={MRole} />
+        <Input />
+      </div>
     </div>
   );
 }
